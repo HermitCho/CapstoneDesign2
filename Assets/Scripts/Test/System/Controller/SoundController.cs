5b8419c4511f6da8caa7b17bfc8b03feb06dfcdb
Minki Cho <130172235+HermitCho@users.noreply.github.com>
2025-07-27 23:24:32 +0900

    Add sound
    상점 및 캐릭터 선택창 사운드 추가

--- Assets/Scripts/Test/System/Controller/SoundController.cs
+++ Assets/Scripts/Test/System/Controller/SoundController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SoundController : Singleton<SoundController>
+public class SoundController : MonoBehaviour
 {
     // Start is called before the first frame update
     public void OnClick()
@@ -18,5 +18,13 @@ public class SoundController : Singleton<SoundController>
     public void OnClickCancel()
     {
         AudioManager.Inst.PlayOneShot("SFX_UI_ClickCancel");
+    }
+    public void OnClickError()
+    {
+        AudioManager.Inst.PlayOneShot("SFX_UI_ClickError");
+    }
+    public void OnClickSelectCharacter()
+    {
+        AudioManager.Inst.PlayOneShot("SFX_UI_SelectCharacter");
     }
 }
