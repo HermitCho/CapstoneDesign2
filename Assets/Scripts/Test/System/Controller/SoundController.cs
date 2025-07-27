f8749ca321ee552e587ac9fb0c9794e7534228dc
BeginnerKJY <wldyd8789@naver.com>
2025-07-27 23:25:02 +0900

    0727-JY-06

--- Assets/Scripts/Test/System/Controller/SoundController.cs
+++ Assets/Scripts/Test/System/Controller/SoundController.cs
@@ -2,12 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SoundController : Singleton<SoundController>
+public class SoundController : MonoBehaviour
 {
     // Start is called before the first frame update
     public void OnClick()
     {
         AudioManager.Inst.PlayOneShot("SFX_UI_Click");
+
     }
 
     public void OnClickMatching()
@@ -18,5 +19,31 @@ public class SoundController : Singleton<SoundController>
     public void OnClickCancel()
     {
         AudioManager.Inst.PlayOneShot("SFX_UI_ClickCancel");
+    }
+
+    public void OnClickMini()
+    {
+        AudioManager.Inst.PlayOneShot("SFX_UI_ClickMini");
+    }
+
+    public void OnSlider()
+    {
+        AudioManager.Inst.PlayOneShot("SFX_UI_Slider");
+    }
+
+
+    public void OnHotKey()
+    {
+        AudioManager.Inst.PlayOneShot("SFX_UI_Hotkey");
+    }
+
+    public void OnClickError()
+    {
+        AudioManager.Inst.PlayOneShot("SFX_UI_ClickError");
+    }
+    
+    public void OnClickSelectCharacter()
+    {
+        AudioManager.Inst.PlayOneShot("SFX_UI_SelectCharacter");
     }
 }
