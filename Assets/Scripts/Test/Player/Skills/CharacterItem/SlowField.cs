d3aca5c5fed1e8fb0909417bf139d811c1a3e8de
Minki Cho <130172235+HermitCho@users.noreply.github.com>
2025-09-09 19:40:24 +0900

    (25.09.09)Add Slowball Item
    투척 아이템 슬로우볼, 투척 후 슬로우볼 충돌 시 바닥에 슬로우 지역 생성

---
+++ Assets/Scripts/Test/Player/Skills/CharacterItem/SlowField.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class SlowField : MonoBehaviour
+{
+    private List<MoveController> slowedPlayers = new List<MoveController>();
+    private float slowAmountMultiplier = 0.5f;
+    private float fieldLifeTime = 5f;
+
+    void Awake()
+    {
+        StartCoroutine(SlowFieldLifetime());
+    }
+
+    [PunRPC]
+    private IEnumerator SlowFieldLifetime()
+    {
+        yield return new WaitForSeconds(fieldLifeTime);
+        Destroy(gameObject);
+    }
+
+    [PunRPC]
+    private void OnTriggerEnter(Collider other)
+    {
+        // 마스터 클라이언트에서만 처리
+        if (!PhotonNetwork.IsMasterClient) return;
+
+        MoveController player = other.GetComponent<MoveController>();
+        if (player != null && !slowedPlayers.Contains(player))
+        {
+            // 속도 감소 적용
+            player.photonView.RPC("SetSpeedMultiplier", RpcTarget.All, slowAmountMultiplier);
+            slowedPlayers.Add(player);
+        }
+    }
+
+    [PunRPC]
+    private void OnTriggerExit(Collider other)
+    {
+        // 마스터 클라이언트에서만 처리
+        if (!PhotonNetwork.IsMasterClient) return;
+
+        MoveController player = other.GetComponent<MoveController>();
+        if (player != null && slowedPlayers.Contains(player))
+        {
+            // 원래 속도로 복구
+            player.photonView.RPC("SetSpeedMultiplier", RpcTarget.All, 1 / slowAmountMultiplier);
+            slowedPlayers.Remove(player);
+        }
+    }
+}
